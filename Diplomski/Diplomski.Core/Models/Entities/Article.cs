cd76f49cb89c15c7ea538a16654fe65e9b6b1da3
Filip Lukinić <filiplukinic@hotmail.com>
2022-04-13 23:42:09 +0200

    Added solution

---
+++ Diplomski/Diplomski.Core/Models/Entities/Article.cs
@@ -0,0 +1,33 @@
+﻿namespace Diplomski.Core.Models.Entities
+{
+    public class Article
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Entry { get; set; }
+
+        public decimal Exit { get; set; }
+
+        public decimal SingularPrice { get; set; }
+
+        public decimal Owe { get; set; }
+
+        public decimal Demand { get; set; }
+
+        public decimal Tariff { get; set; }
+
+        public string PLA { get; set; }
+
+        public decimal OP { get; set; }
+
+        public decimal Rebate { get; set; }
+
+        public decimal N { get; set; }
+
+        public decimal BuyPrice { get; set; }
+
+        public decimal Tax { get; set; }
+    }
+}
