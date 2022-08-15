838c06038cf5c1b98e023805b0a144b7cd647b73
Filipkec1 <filiplukinic@hotmail.com>
2022-08-15 18:53:48 +0200

    feat(-): Added Article enum

---
+++ src/Diplomski.Core/Models/Enums/ArticleEnums.cs
@@ -0,0 +1,14 @@
+﻿namespace Diplomski.Core.Models.Enums
+{
+    public enum PlaEnum
+    {
+        CEK,
+        GOT,
+        K00,
+        K12,
+        K33,
+        K35,
+        K39,
+        KRD
+    }
+}
